4786fa2e72c6aff43ca74e30780d774e71429125
Beto-23 <mansilla_alberto@outlook.com>
2020-04-08 02:17:01 -0300

    Iniciando el desarrollo de reucers

---
+++ src/icons/MovieIcon.js
@@ -0,0 +1,11 @@
+import {svgIcon, SvgIcon} from '@material-ui/core';
+import React from 'react';
+import LocalMoviesTwoToneIcon from '@material-ui/icons/LocalMoviesTwoTone';
+
+
+
+export default props => (
+    <SvgIcon {...props}>
+        <LocalMoviesTwoToneIcon></LocalMoviesTwoToneIcon>
+    </SvgIcon>
+);
